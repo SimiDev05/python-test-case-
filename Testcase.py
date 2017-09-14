d3460fe8f1cbc4bea1bd6f6814a793b3bf790b6a
SIMI <demogit222@gmail.com>
2017-09-14 11:35:55 +0700

    SECOND COMMIT

--- Testcase.py
+++ Testcase.py
@@ -13,28 +13,47 @@
 # age = 24
 # print(name + str(age))
 
-parrot = "wonderland"
-print(parrot)
-print(parrot[3])    #call bracket ,start count from zero
-print(parrot[-3])
-print(parrot[0:3])
-print(parrot[-5:-2])
-print(parrot[0:8:2])
-number = "1, 2, 3, 4, 5, 6,"
-print(number[0::3])
+# parrot = "wonderland"
+# print(parrot)
+# print(parrot[3])    #call bracket ,start count from zero
+# print(parrot[-3])
+# print(parrot[0:3])
+# print(parrot[-5:-2])
+# print(parrot[0:8:2])
+# number = "1, 2, 3, 4, 5, 6,"
+# print(number[0::3])
+#
+# print("he's " "probably "  "pining")
+# print("he's probably pining")
+# print("hello " * 5)
+# print("land" in parrot)
 
-print("he's " "probably "  "pining")
-print("he's probably pining")
-print("hello " * 5)
-print("land" in parrot)
+# print('there are 31 days in jan feb nov dec')
+# jane = 'jan'
+# faith = 'feb'
+# _1st = 'mar'
+# no = 31
+# print('there are' + ' ' + str(no) + ' ' + 'days in' + ' ' + jane + ' ' + faith + ' ' + _1st)
+# print('there are {0} days in {1}, {2}, {3}' .format(no, jane, faith, _1st))
+# print('my age is {0} years and {1}' .format(24, 6))
+# for i in range(1, 12):
+#     print('No. {0:2} squared is {1:4} and cubed is {2:2}' .format(i, i**2, i**3))
+# print(' Pi is aprox {0} and area is {1}' .format(22/7, 22*7))
 
-print('there are 31 days in jan feb nov dec')
-jane = 'jan'
-faith = 'feb'
-_1st = 'mar'
-no = 31
-print('there are' + ' ' + str(no) + ' ' + 'days in' + ' ' + jane + ' ' + faith + ' ' + _1st)
-print('there are {0} days in {1}, {2}, {3}' .format(no, jane, faith, _1st))
-print('my age is {0} years and {1}' .format(24, 6))
-for i in range(1, 12):
-    print('No. {0} squared is {1} and cubed is {2}' .format(i, i**2, i**3))
+# programflow
+
+for i in range(1, 5):
+    print('{0} is equals {1}' .format(i, i/2))
+    print('calc. complete')
+    print('please try again')
+
+name = str(input('hello \nplease enter your name: '))
+print('nice to meet you {0}' .format(name))
+age = int(input('how old are you {0}: ' .format(name)))
+print('{0} you just said you are {1} years old' .format(name, age))
+
+if age >= 18:
+    print('{0} your are old enough to enter this site' .format(name))
+    print('WELCOME')
+else:
+    print('{0} please come back in {1} years time' .format(name, 18-age))
