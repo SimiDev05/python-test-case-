81e8428d0a124eef9f0bf8faf340e2f93d83c237
SIMI <demogit222@gmail.com>
2017-09-14 17:27:48 +0700

    SECOND COMMIT

--- Testcase.py
+++ Testcase.py
@@ -58,24 +58,57 @@
 # else:
 #     print('{0} please come back in {1} years time' .format(name, 18-age))
 
-print('please guess a number')
-guess = int(input())
-if guess < 5:
-    print('please guess higher')
-    int(input())
-    if guess == 5:
-        print('Correct')
-    else:
-        print('Wrong')
-elif guess > 5:
-        print('please guess lower')
-        int(input())
-        if guess == 5:
-            print('Correct')
-        else:
-            print('Wrong')
-else:
-    print('you are smart you guessed right the first time')
+# print('please guess a number between 1 and 10  ')
+# guess = int(input())
+# if guess != 5:
+#     if guess < 5:
+#         print('please guess higher')
+#     else:
+#         print('please guess lower')
+#     guess = int(input())
+#     if guess == 5:
+#         print('Correct')
+#     else:
+#         print('Wrong')
+# else:
+#     print('you are smart you guessed right the first time')
 
+
+# x = "simo"
+# if x:
+#     print('x is true')
+# print("""False: {0}
+#  2 None: {1}
+#  3 0: {2}
+#  4 0.0: {3}
+#  5 empty list: {4}
+#  6 empty tuple(): {5)
+#  7 empty str: {6}
+#  8 empty mapping: {7}""" .format(False, bool(None), bool(0), bool(0.0),
+# bool([]), bool(()), bool(''), bool({})))
+
+# note = input('please enter some text    ')
+# if note:
+#     print('you entered "{}"' .format(note))
+# else:
+#     print('you did not enter anything')
+
+# lib = ['ball cat neck moon sea man boy']
+# word = str(input('please guess the word'))
+# if word in lib:
+#     print('correct guess')
+# else:
+#     print('sorry try again later')
+
+c = 10
+while c != 0:
+    print(c)
+    c -= 1
+
+while True:
+    response = int(input('please enter a number divisible by 2'))
+    if response % 2 == 0:
+        print('Good Job')
+        break
 
 
